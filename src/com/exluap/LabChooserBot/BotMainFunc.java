c4d3d871e2c3c6d6647e62018f63e480e7443b1b
exluap <nickzaytsew@gmail.com>
2016-12-28 00:06:30 +0300

    * Изменил алгоритм ввода данных. Теперь не надо писать огромное предложение, чтобы узнать свой вариант. * Добавил пасхалку для одногруппника

--- src/com/exluap/LabChooserBot/BotMainFunc.java
+++ src/com/exluap/LabChooserBot/BotMainFunc.java
@@ -2,10 +2,13 @@ package com.exluap.LabChooserBot;
 
 import com.vdurmont.emoji.EmojiParser;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.io.File;
 
 /**
  * com.exluap.LabChooserBot
@@ -38,14 +41,23 @@ public class BotMainFunc extends TelegramLongPollingBot {
         if (message.getText().equals("/start")) {
             SendMSG(message.getChatId().toString(), EmojiParser.parseToUnicode("Привет! :hand: Я бот, который поможет тебе узнать твой вариант для лабораторных" +
                     "работ по программированию №2 и №3" +
-                    " Напиши мне последние 4 цифры твоего студентческого с текстом `Мои цифры студенческого: ` " +
+                    " Напиши мне последние 4 цифры твоего студенческого " +
                     "и я верну номера вариантов :memo:" +
-                    "Меня разработал :nerd: @exluap" +
-                    " P.S. Помни, я очень чувствителен к регистру!!! так что будь осторожен при наборе команды `Мои цифры студенческого: `"));
-        } else if (message.getText().startsWith("Мои цифры студенческого:") ) {
-            SendMSG(message.getChatId().toString(),"Твой вариант лабораторной №2 : " + LabWork(Integer.parseInt(splitter(message.getText())))); //Преобразование строки в число
-            SendMSG(message.getChatId().toString(),"Твой вариант лабораторной №3 : " + LabWork2(Integer.parseInt(splitter(message.getText()))));
-            SendMSG(message.getChatId().toString(),LabWork4(Integer.parseInt(splitter(message.getText()))));
+                    "Меня разработал :nerd: @exluap"));
+        } else if (message.getText().matches("[-+]?\\d+") && message.getText().length() == 4) {
+            SendMSG(message.getChatId().toString(),"Твой вариант лабораторной №2 : " + LabWork(Integer.parseInt(message.getText()))); //Преобразование строки в число
+            SendMSG(message.getChatId().toString(),"Твой вариант лабораторной №3 : " + LabWork2(Integer.parseInt(message.getText())));
+            SendMSG(message.getChatId().toString(),LabWork4(Integer.parseInt(message.getText())));
+        } else if (message.getText().equalsIgnoreCase("слава Украине!")) {
+            UkraineLifeHack(message.getChatId().toString());
+        } else if(!message.getText().matches("[-+]?\\d+")){
+            SendMSG(message.getChatId().toString(), EmojiParser.parseToUnicode("Прости меня, но я понимаю только цифры. Напиши мне последние 4 цифры студенческого и я скажу тебе твой вариант. Не надо писать студенческий номер полностью" +
+                    ", пожалей мою оперативную память, ведь мой хозяин, @exluap, написал меня на Java :sweat:" +
+                    ", давай попробуем ввод еще разок?"));
+        }else if (message.getText().length() !=4) {
+            SendMSG(message.getChatId().toString(), EmojiParser.parseToUnicode("Упс :fearful:! Что-то пошло не так, " +
+                    "я насчитал больше 4 символов, быть может ты ввел лишний пробел или написал номер студнческого" +
+                    " полностью? Давай попробуем снова? :wink:"));
         }
 
         }
@@ -110,5 +122,17 @@ public class BotMainFunc extends TelegramLongPollingBot {
             case 9: return result + (numbers-3);
         }
       return null;
+    }
+
+    void UkraineLifeHack(String ChatID) {
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setChatId(ChatID);
+        File photo = new File("res/Ukraine.jpg");
+        sendPhoto.setNewPhoto(photo);
+        try {
+            sendPhoto(sendPhoto);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
